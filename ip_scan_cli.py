8a422eca125b83b2c5881f18f4ee09619f628fc4
Rock0401 <Rock.Shih@gigacomputing.com>
2025-04-14 11:47:00 +0800

    If the password entered is incorrect, it will try the PWs from the text file, if it exists. (Max pw:5. Over 5 incorrect tries might be banned by BMC.)

--- ip_scan_cli.py
+++ ip_scan_cli.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import urllib3
 from requests.auth import HTTPBasicAuth
 import concurrent.futures
+import time
 
 #disable warning of SSL
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -18,7 +19,7 @@ def get_ip_input(prompt, default):
         return user_input
     
     if user_input.isdigit() and 0 <= int(user_input) <= 255:  # host addr only
-        return f"10.1.1.{user_input}"
+        return f"10.1.111.{user_input}"
     
     print("Invalid input. Using default IP:", default)
     return default
@@ -44,12 +45,14 @@ def print_banner():
         "Starting IP can be full IP (e.g., 10.2.1.1) or last octet (e.g., 1 for 10.1.111.1) (default:2)",
         "Ending IP should be the last octet (1 to 254, default:250)",
         "",
+        "For any issues, please contact Rock",
+    ]
+    """
+        "",
         "Timeout:",
         "set to 0.5 seconds for faster scans (may miss some responses sometimes).",
         "set to 1 seconds for more stability (slower).",
-        "For any issues, please contact Rock",
-    ]
-
+    """
     max_length = max(len(line) for line in banner_text)
     print("*" * (max_length + 4))
     for line in banner_text:
@@ -57,22 +60,40 @@ def print_banner():
     print("*" * (max_length + 4) ,"\n")
 
 ####################################### Scan Function ###########################################
-def scan_ip(target_ip, username, password, timeout):
+def scan_ip(target_ip, username, password, timeout, password_list):
     target_url = f"http://{target_ip}/redfish/v1/Chassis/1"
-    try:
-        response = requests.get(target_url, auth=HTTPBasicAuth(username,password), verify=False, timeout=float(timeout))
-        #verify=False to disable SSL warning/error
-        if response.status_code == 200:
-            chassis_data = response.json()
-            model = chassis_data.get("Model", "No Model column")
-            sn = chassis_data.get("Oem", {}).get("GBTChassisOemProperty", {}).get("Board Serial Number", "No SerialNumber column")
-            print(f"IP:{target_ip.ljust(20)} {model.ljust(30)}"+f"SN:{sn.ljust(25)}")
-            return target_ip, model, sn
-        else:
-            return target_ip, "Login Fail", "N/A"
-    except requests.exceptions.RequestException:
-        return target_ip, "Cannot connect.", "N/A"
 
+    try_passwords = [password] + password_list.copy()
+    for pw in try_passwords:
+        try:
+            response = requests.get(target_url, auth=HTTPBasicAuth(username,pw), verify=False, timeout=float(timeout))
+            #verify=False to disable SSL warning/error
+            if response.status_code == 200:
+
+                chassis_data = response.json()
+                model = chassis_data.get("Model", "No Model column")
+                sn = chassis_data.get("Oem", {}).get("GBTChassisOemProperty", {}).get("Board Serial Number", "No SerialNumber column")
+                print(f"IP:{target_ip.ljust(20)} {model.ljust(30)}"+f"SN:{sn.ljust(25)}")
+                if model=="No Model column":
+                    continue
+                return target_ip, model, sn
+            elif response.status_code == 401:
+                time.sleep(0.2)
+        except requests.exceptions.RequestException as e:
+            continue
+        except KeyboardInterrupt:
+            print("\nUser interrupted the program. Exiting gracefully.")
+        time.sleep(0.05)
+    return target_ip, "Cannot connect.", "N/A"
+        
+####################################### Load password.txt Function ###########################################
+def load_password_list(file_path="password.txt"):
+    try:
+        with open(file_path, 'r') as file:
+            return [line.strip() for line in file if line.strip()]
+    except FileNotFoundError:
+        print("Password file not found. Skipping alternate password attempts.")
+        return []
 
 ####################################### Main Function ###########################################
 def main():
@@ -84,7 +105,11 @@ def main():
         
         scan_ip_start=get_ip_input(("Please enter starting IP for scan range (default: 2):"), "10.1.111.2")
         scan_ip_end=int(get_limited_input("Please enter ending IP for scan range (default: 250, last octet only):",254,250))
-        timeout=float(get_limited_input("Please enter timeout (default: 0.6):",10,0.6) or 0.6)
+        #timeout=float(get_limited_input("Please enter timeout (default: 0.6):",10,0.6) or 0.6)
+        timeout=0.6
+        
+        password_list = load_password_list()
+        print("Passwords:",password_list)
 
         #handling input for IP range
         parts=scan_ip_start.split(".")
@@ -95,11 +120,11 @@ def main():
         print(" ")
         #Start scanning
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: #using ThreadPoolExecutor for faster scan
-            results=executor.map(lambda ip: scan_ip(ip,username,password,timeout),ip_range)
+            results=executor.map(lambda ip: scan_ip(ip,username,password,timeout,password_list),ip_range)
             for target_ip,model,sn in results:
                 if model != "Cannot connect." and model != "Login Fail":
                     valid_ip.append((target_ip,model,sn))
-        #for ip,model,sn in valid_ip:
+        #for ip,model,sn in valid_ip:          #print the result in order
         #    print(f"IP:{ip.ljust(20)} {model.ljust(30)}"+f"SN:{sn.ljust(25)}")
         print("Found",len(valid_ip),"devices")
         input("Press Enter to exit...")
